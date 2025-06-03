3d65bd1729f0727aedb0037d27906b37fafadd83
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-02 20:17:59 -0700

    add interfaces to the port list of the async_fifo

--- lab1/async_fifo/src/async_fifo.sv
+++ lab1/async_fifo/src/async_fifo.sv
@@ -4,22 +4,8 @@ module async_fifo #(
     parameter int DATA_WIDTH = 8,
     parameter int ADDR_WIDTH = 8
 ) (
-    // Push interface
-    input  logic                  wclk,
-    input  logic                  wen,
-    input  logic [DATA_WIDTH-1:0] wdata,
-    output logic                  full,
-    output logic                  almost_full,
-
-    // Pop interface
-    input  logic                  rclk,
-    input  logic                  ren,
-    output logic [DATA_WIDTH-1:0] rdata,
-    output logic                  empty,
-    output logic                  almost_empty,
-
-    // Global reset
-    input  logic                  rst_n
+    read_if  #(DATA_WIDTH) read,
+    write_if #(DATA_WIDTH) write
 );
 
     // Internal signals for read/write pointers
@@ -32,49 +18,49 @@ module async_fifo #(
 
     // Create memory block
     memory #(DATA_WIDTH, ADDR_WIDTH) mem1(
-        .wclk(wclk),
-        .wen(wen),
-        .full(full),
+        .wclk(write.clk),
+        .wen(write.en),
+        .full(write.full),
         .waddr(waddr[ADDR_WIDTH-1:0]),
         .raddr(raddr[ADDR_WIDTH-1:0]),
-        .wdata(wdata),
-        .rdata(rdata)
+        .wdata(write.data),
+        .rdata(read.data)
     );
 
     // Compute the read pointer and empty status flags
     read_pointer #(ADDR_WIDTH+1) rptr_empty(
-        .rclk(rclk),
-        .rst_n(rst_n),
-        .ren(ren),
+        .rclk(read.clk),
+        .rst_n(read.rst_n),
+        .ren(read.en),
         .wptr_sync(wptr_sync),
         .raddr(raddr),
         .rptr(rptr),
-        .empty(empty),
-        .almost_empty(almost_empty)
+        .empty(read.empty),
+        .almost_empty(read.almost_empty)
     );
 
     // Compute the write pointer and full status flags
     write_pointer #(ADDR_WIDTH+1) wptr_full(
-        .wclk(wclk),
-        .rst_n(rst_n),
-        .wen(wen),
+        .wclk(write.clk),
+        .rst_n(write.rst_n),
+        .wen(write.en),
         .rptr_sync(rptr_sync),
         .waddr(waddr),
         .wptr(wptr),
-        .full(full),
-        .almost_full(almost_full)
+        .full(write.full),
+        .almost_full(write.almost_full)
     );
 
     // Synchronize the pointers with clock domain crossing
     synchronizer #(ADDR_WIDTH+1) rsync(
-        .clk(wclk),
-        .rst_n(rst_n),
+        .clk(write.clk),
+        .rst_n(write.rst_n),
         .d(rptr),
         .q2(rptr_sync)
     );
     synchronizer #(ADDR_WIDTH+1) wsync(
-        .clk(rclk),
-        .rst_n(rst_n),
+        .clk(read.clk),
+        .rst_n(read.rst_n),
         .d(wptr),
         .q2(wptr_sync)
     );
---
+++ lab1/async_fifo/src/interfaces.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+interface read_if #(
+    parameter DATA_WIDTH = 8
+);
+
+    logic                  clk;
+    logic                  rst_n;
+    logic                  en;
+    logic [DATA_WIDTH-1:0] data;
+    logic                  empty;
+    logic                  almost_empty;
+
+endinterface
+
+interface write_if #(
+    parameter DATA_WIDTH = 8
+);
+
+    logic                  clk;
+    logic                  rst_n;
+    logic                  en;
+    logic [DATA_WIDTH-1:0] data;
+    logic                  full;
+    logic                  almost_full;
+
+endinterface
