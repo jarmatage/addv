9add92310ae68f9acc7c324a2f1acc320e568a15
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-03 09:12:07 -0700

    add a reset task

--- lab1/async_fifo/src/tb_async_fifo.sv
+++ lab1/async_fifo/src/tb_async_fifo.sv
@@ -37,27 +37,30 @@ module tb_async_fifo;
     initial rclk = 0;
     always #8 rclk = ~rclk; // 16ns period = 62.5MHz
 
-    // Reset the DUT
-    initial begin
-        rst_n = 0;
-        wen   = 0;
-        ren   = 0;
-        wdata = 0;
-        #40; // wait 5 read clock cycles then deassert the reset
-        rst_n = 1;
-        #40;
-    end
-
     // Main test sequence
     initial begin
+        reset();
         check_reset();
         fill_fifo();
         empty_fifo();
         simultaneous_rw();
         #40;
-        $fsdbDumpvars(); // Create a waveform FSDB for Verdi
+        //$fsdbDumpvars(); // Create a waveform FSDB for Verdi
         $finish;
     end
+
+    // Reset the DUT
+    task automatic reset();
+        rst_n = 0;
+        wen   = 0;
+        ren   = 0;
+        wdata = 0;
+        wcnt  = 0;
+        rcnt  = 0;
+        #40; // wait 5 read clock cycles then deassert the reset
+        rst_n = 1;
+        #40;
+    endtask
 
     // Push data into FIFO
     task automatic push();
@@ -69,12 +72,11 @@ module tb_async_fifo;
     endtask
 
     // Pop data from the FIFO
-    task automatic pop(output logic [DATA_WIDTH-1:0] data);
+    task automatic pop();
         ren <= 1'b1;
         if (!empty) rcnt <= rcnt + 8'd1;
         @(posedge rclk);
         ren <= 1'b0;
-        data = rdata;
     endtask
 
     // Check the FIFO's initial reset state
@@ -95,7 +97,7 @@ module tb_async_fifo;
 
     // Write to the FIFO until it is full
     task automatic fill_fifo();
-        $display("\n--- Filling the FIFO ---\n")
+        $display("\n--- Filling the FIFO ---\n");
 
         repeat (DATA_DEPTH) begin
             push();
@@ -121,7 +123,7 @@ module tb_async_fifo;
             end
 
             // Display each iteration
-            $display("write %0d: full=%0b, almost_full=%0b", wcnt, full, almost_full)
+            $display("write %0d: full=%0b, almost_full=%0b", wcnt, full, almost_full);
         end
 
         // Perform one extra write attempt
@@ -134,7 +136,7 @@ module tb_async_fifo;
 
     // Read from the FIFO until it is empty
     task automatic empty_fifo();
-        $display("\n--- Emptying the FIFO ---\n")
+        $display("\n--- Emptying the FIFO ---\n");
 
         repeat (DATA_DEPTH) begin
             pop();
@@ -146,7 +148,7 @@ module tb_async_fifo;
             end
 
             // Check that the almost empty flag is correct
-            if (rcnt >= (DEPTH * 3/4) && !almost_empty) begin
+            if (rcnt >= (DATA_DEPTH * 3/4) && !almost_empty) begin
                 $display("ERROR: almost_empty flag did not assert at rcnt=%0d and time=%t", rcnt, $time);
                 $finish;
             end
