3f3638994766390dcf9b1d7868f92f3b96cc8032
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 00:28:04 -0700

    fix bitwidths in the regfile

--- lab2/mips_pipelined/src/regfile.sv
+++ lab2/mips_pipelined/src/regfile.sv
@@ -10,6 +10,6 @@ module regfile(
     always_ff @(posedge clk)
         if (we3) rf[wa3] <= wd3;
 
-    assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
-    assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+    assign rd1 = (ra1 != 0) ? rf[ra1] : '0;
+    assign rd2 = (ra2 != 0) ? rf[ra2] : '0;
 endmodule
