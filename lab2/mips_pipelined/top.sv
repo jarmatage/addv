956da66c76ce296105110af438ee5cd31ca6bc13
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 00:27:47 -0700

    move the dmem and imem into the top.sv file

--- lab2/mips_pipelined/top.sv
+++ lab2/mips_pipelined/top.sv
@@ -1,17 +1,55 @@
-module top(
+module top (
     input  logic        clk, reset,
     output logic [31:0] writedata,
     output logic [31:0] dataadr,
     output logic        memwrite
 );
-    logic [31:0] pc, instr, readdata;
+    wire [31:0] pc, instr, readdata;
 
-    // Instantiate modules
-    mips mips_core(.clk(clk), .reset(reset), .pc(pc), .instr(instr),
-                   .memwrite(memwrite), .dataadr(dataadr),
-                   .writedata(writedata), .readdata(readdata));
+    // instantiate processor and memories
+    mips mips (
+        .clk,
+        .reset,
+        .pc,
+        .instr,
+        .memwrite,
+        .aluout(dataadr),
+        .writedata,
+        .readdata
+    );
+    imem imem (
+        .a(pc[7:2]),
+        .rd(instr)
+    );
+    dmem dmem (
+        .clk, 
+        .we(memwrite),
+        .a(dataadr),
+        .wd(writedata),
+        .rd(readdata)
+    );
+endmodule
 
-    imem imem_inst(.a(pc[7:2]), .rd(instr));
-    dmem dmem_inst(.clk(clk), .we(memwrite), .a(dataadr),
-                   .wd(writedata), .rd(readdata));
+
+module dmem(
+    input  logic        clk,
+    input  logic        we,
+    input  logic [31:0] a,
+    input  logic [31:0] wd,
+    output logic [31:0] rd
+);
+    logic [31:0] RAM[63:0];
+    assign rd = RAM[a[31:2]];
+    always_ff @(posedge clk)
+        if (we) RAM[a[31:2]] <= wd;
+endmodule
+
+
+module imem(
+    input  logic [5:0]  a,
+    output logic [31:0] rd
+);
+    logic [31:0] RAM[63:0];
+    initial $readmemh("../memfile.dat", RAM);
+    assign rd = RAM[a];
 endmodule
