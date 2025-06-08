4656b0aed4b892808c691de00729d76b49839df6
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 00:28:23 -0700

    fix bitwidth in the alu

--- lab2/mips_pipelined/src/alu.sv
+++ lab2/mips_pipelined/src/alu.sv
@@ -6,12 +6,12 @@ module alu(
 );
     always_comb begin
         unique case(control)
-            3'b000: result = a & b;          // AND
-            3'b001: result = a | b;          // OR
-            3'b010: result = a + b;          // ADD
-            3'b110: result = a - b;          // SUB
-            3'b111: result = ($signed(a) < $signed(b));
-            default: result = 32'hx;
+            3'b000: result = a & b;                     // AND
+            3'b001: result = a | b;                     // OR
+            3'b010: result = a + b;                     // ADD
+            3'b110: result = a - b;                     // SUB
+            3'b111: result = ($signed(a) < $signed(b)); // SLT (signed)
+            default: result = 'x;
         endcase
     end
     assign zero = (result == 32'd0);
