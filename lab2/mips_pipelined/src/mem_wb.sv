3cdec172aa4cb8ea3a9f9d27deb6ac69562f3f52
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 00:56:01 -0700

    improve the bitwidth handling in the mem_wb

--- lab2/mips_pipelined/src/mem_wb.sv
+++ lab2/mips_pipelined/src/mem_wb.sv
@@ -15,13 +15,14 @@ module mem_wb(
     output logic [31:0] readdata_out,
     output logic [4:0]  destReg_out
 );
+
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            memtoreg_out <= 0;
-            regwrite_out <= 0;
-            aluout_out   <= 0;
-            readdata_out <= 0;
-            destReg_out  <= 0;
+            memtoreg_out <= '0;
+            regwrite_out <= '0;
+            aluout_out   <= '0;
+            readdata_out <= '0;
+            destReg_out  <= '0;
         end else begin
             memtoreg_out <= memtoreg_in;
             regwrite_out <= regwrite_in;
