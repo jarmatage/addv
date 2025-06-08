464562677e638f4e941823a8b0d18b205874aabc
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 08:55:15 -0700

    remove the zero and pcsrc signals from the controller to move them to the datapath

--- lab2/mips_pipelined/src/controller.sv
+++ lab2/mips_pipelined/src/controller.sv
@@ -1,14 +1,12 @@
 module controller (
     input  logic [5:0] op, funct,
-    input  logic       zero,
     output logic       memtoreg, memwrite,
-    output logic       pcsrc, alusrc,
+    output logic       branch, alusrc,
     output logic       regdst, regwrite,
     output logic       jump,
     output logic [2:0] alucontrol
 );
     wire [1:0] aluop;
-    wire branch;
     
     maindec md(
         .op,
@@ -26,8 +24,6 @@ module controller (
         .aluop,
         .alucontrol
     );
-    
-    assign pcsrc = branch & zero;
 endmodule
 
 
