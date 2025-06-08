1e1ed3fcb63d13667d6884000d82ae944575e8af
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-08 11:17:00 -0700

    convert non-blocking to blocking in always_comb

--- lab2/mips_pipelined/src/controller.sv
+++ lab2/mips_pipelined/src/controller.sv
@@ -42,13 +42,13 @@ module maindec(
 
     always_comb begin
         case(op)
-            6'b000000: controls <= 9'b110000010; //Rtyp
-            6'b100011: controls <= 9'b101001000; //LW
-            6'b101011: controls <= 9'b001010000; //SW
-            6'b000100: controls <= 9'b000100001; //BEQ
-            6'b001000: controls <= 9'b101000000; //ADDI
-            6'b000010: controls <= 9'b000000100; //J
-            default:   controls <= 9'bxxxxxxxxx; //???
+            6'b000000: controls = 9'b110000010; //Rtyp
+            6'b100011: controls = 9'b101001000; //LW
+            6'b101011: controls = 9'b001010000; //SW
+            6'b000100: controls = 9'b000100001; //BEQ
+            6'b001000: controls = 9'b101000000; //ADDI
+            6'b000010: controls = 9'b000000100; //J
+            default:   controls = 9'bxxxxxxxxx; //???
         endcase
     end
 endmodule
@@ -63,21 +63,21 @@ module aludec (
     logic [2:0] rtype;
 
     always_comb begin
-        unique case (funct) 
-            6'b100000: rtype <= 3'b010; // ADD
-            6'b100010: rtype <= 3'b110; // SUB
-            6'b100100: rtype <= 3'b000; // AND
-            6'b100101: rtype <= 3'b001; // OR
-            6'b101010: rtype <= 3'b111; // SLT
-            default:   rtype <= 3'bxxx; // ???
+        case (funct) 
+            6'b100000: rtype = 3'b010; // ADD
+            6'b100010: rtype = 3'b110; // SUB
+            6'b100100: rtype = 3'b000; // AND
+            6'b100101: rtype = 3'b001; // OR
+            6'b101010: rtype = 3'b111; // SLT
+            default:   rtype = 3'bxxx; // ???
         endcase
     end
 
     always_comb begin
-        unique case (aluop)
-            2'b00:   alucontrol <= 3'b010; // add
-            2'b01:   alucontrol <= 3'b110; // sub
-            default: alucontrol <= rtype;  // RTYPE
+        case (aluop)
+            2'b00:   alucontrol = 3'b010; // add
+            2'b01:   alucontrol = 3'b110; // sub
+            default: alucontrol = rtype;  // RTYPE
         endcase
     end
 endmodule
