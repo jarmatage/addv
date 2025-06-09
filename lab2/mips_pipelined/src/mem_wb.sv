2857e97bb5e94fdcf117f1afc71182b1b74dbea7
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-09 15:17:33 -0700

    add branch to mem_wb

--- lab2/mips_pipelined/src/mem_wb.sv
+++ lab2/mips_pipelined/src/mem_wb.sv
@@ -2,6 +2,7 @@ module mem_wb(
     input  logic        clk,
     input  logic        reset,
     // Control in
+    input  logic        branch_MEM,
     input  logic        memtoreg_MEM,
     input  logic        regwrite_MEM,
     // Data in
@@ -9,6 +10,7 @@ module mem_wb(
     input  logic [31:0] readdata_MEM,
     input  logic [4:0]  writereg_MEM,
     // outputs
+    output logic        branch_WB,
     output logic        memtoreg_WB,
     output logic        regwrite_WB,
     output logic [31:0] aluout_WB,
@@ -18,12 +20,14 @@ module mem_wb(
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
+            branch_WB   <= '0;
             memtoreg_WB <= '0;
             regwrite_WB <= '0;
             aluout_WB   <= '0;
             readdata_WB <= '0;
             writereg_WB <= '0;
         end else begin
+            branch_WB   <= branch_MEM;
             memtoreg_WB <= memtoreg_MEM;
             regwrite_WB <= regwrite_MEM;
             aluout_WB   <= aluout_MEM;
