deadd0b276e47cfc3e18a33b337c31a5d108e0e5
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-11 21:29:02 -0700

    copy over fpmult code

---
+++ lab3/fp8/src/FPMult_NormalizeModule.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Create Date:    12:47:06 09/20/2012 
+// Module Name:    FPMult_NormalizeModule 
+// Project Name: 	 Floating Point Project
+// Author:			 Fredrik Brosser
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module FPMult_NormalizeModule(
+		NormM,
+		NormE,
+		RoundE,
+		RoundEP,
+		RoundM,
+		RoundMP
+    );
+
+	// Input Ports
+	input [22:0] NormM ;									// Normalized mantissa
+	input [8:0] NormE ;									// Normalized exponent
+
+	// Output Ports
+	output [8:0] RoundE ;
+	output [8:0] RoundEP ;
+	output [23:0] RoundM ;
+	output [23:0] RoundMP ; 
+	
+	assign RoundE = NormE - 127 ;
+	assign RoundEP = NormE - 126 ;
+	assign RoundM = NormM ;
+	assign RoundMP = NormM + 1 ;
+
+endmodule
---
+++ lab3/fp8/src/FPMult_RoundModule.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Create Date:    17:14:54 09/20/2012 
+// Module Name:    FPMult_RoundModule 
+// Project Name: 	 Floating Point Project
+// Author:			 Fredrik Brosser
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module FPMult_RoundModule(
+		RoundM,
+		RoundMP,
+		RoundE,
+		RoundEP,
+		Sp,
+		GRS,
+		InputExc,
+		Z,
+		Flags
+    );
+
+	// Input Ports
+	input [23:0] RoundM ;									// Normalized mantissa
+	input [23:0] RoundMP ;									// Normalized exponent
+	input [8:0] RoundE ;									// Normalized mantissa + 1
+	input [8:0] RoundEP ;									// Normalized exponent + 1
+	input Sp ;												// Product sign
+	input GRS ;
+	input [4:0] InputExc ;
+	
+	// Output Ports
+	output [31:0] Z ;										// Final product
+	output [4:0] Flags ;
+	
+	// Internal Signals
+	wire [8:0] FinalE ;									// Rounded exponent
+	wire [23:0] FinalM;
+	wire [23:0] PreShiftM;
+	
+	assign PreShiftM = GRS ? RoundMP : RoundM ;	// Round up if R and (G or S)
+	
+	// Post rounding normalization (potential one bit shift> use shifted mantissa if there is overflow)
+	assign FinalM = (PreShiftM[23] ? {1'b0, PreShiftM[23:1]} : PreShiftM[23:0]) ;
+	
+	assign FinalE = (PreShiftM[23] ? RoundEP : RoundE) ; // Increment exponent if a shift was done
+	
+	assign Z = {Sp, FinalE[7:0], FinalM[22:0]} ;   // Putting the pieces together
+	assign Flags = InputExc[4:0];
+
+endmodule
