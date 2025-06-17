d4beb19c7777b4b77d2ce262406d13a4e37f3e96
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-16 20:14:17 -0700

    add fp8 tasks

--- lab3/tb/matmul_with_ram_tb.sv
+++ lab3/tb/matmul_with_ram_tb.sv
@@ -68,14 +68,31 @@ module matmul_tb;
             $display("Dumping to FSDB");
             $fsdbDumpvars();
         `endif
+
+        // Perform int8 test
         set_matrices_int8();
+        display_inputs_int8();
+        is_fp8 = 1'b0;
         start = 1'b0;
         #115;
         start = 1'b1;
         @(posedge done);
         start = 1'b0;
         #100;         
-        display_output();
+        display_output_int8();
+
+        // Perform fp8 test
+        // set_matrices_fp8();
+        // display_inputs_fp8();
+        // is_fp8 = 1'b1;
+        // start = 1'b0;
+        // #115;
+        // start = 1'b1;
+        // @(posedge done);
+        // start = 1'b0;
+        // #100;         
+        // display_output_fp8();
+
         $finish;
     end
     
@@ -105,13 +122,90 @@ module matmul_tb;
         u_matmul.matrix_B.ram[0]  = {8'h00, 8'h03, 8'h01, 8'h01};
     endtask
 
-    task automatic display_output();
+    //  A           B        Output       Output in hex
+    // 8 4 6 8   1 1 3 0   98 90 82 34    62 5A 52 22
+    // 3 3 3 7   0 1 4 3   75 63 51 26    4B 3F 33 1A
+    // 5 2 1 6   3 5 3 1   62 48 44 19    3E 30 2C 13
+    // 9 1 0 5   9 6 3 2   54 40 46 13    36 28 2E 0D
+    task automatic set_matrices_fp8();
+        //A is stored in ROW MAJOR format
+        //A[0][0] (8'h08) should be the least significant byte of ram[0]
+        //The first column of A should be read together. So, it needs to be 
+        //placed in the first matrix_A ram location.
+        //This is due to Verilog conventions declaring {MSB, ..., LSB}
+        u_matmul.matrix_A.ram[3]  = {8'h05, 8'h06, 8'h07, 8'h08}; 
+        u_matmul.matrix_A.ram[2]  = {8'h00, 8'h01, 8'h03, 8'h06};
+        u_matmul.matrix_A.ram[1]  = {8'h01, 8'h02, 8'h03, 8'h04};
+        u_matmul.matrix_A.ram[0]  = {8'h09, 8'h05, 8'h03, 8'h08};
+
+        //B is stored in COL MAJOR format
+        //B[0][0] (8'h01) should be the least significant of ram[0]
+        //The first row of B should be read together. So, it needs to be 
+        //placed in the first matrix_B ram location. 
+        u_matmul.matrix_B.ram[3]  = {8'h02, 8'h03, 8'h06, 8'h09};
+        u_matmul.matrix_B.ram[2]  = {8'h01, 8'h03, 8'h05, 8'h03};
+        u_matmul.matrix_B.ram[1]  = {8'h03, 8'h04, 8'h01, 8'h00};
+        u_matmul.matrix_B.ram[0]  = {8'h00, 8'h03, 8'h01, 8'h01};
+    endtask
+
+    task automatic display_inputs_int8();
+        $display("\nA =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %d", u_matmul.matrix_A.ram[j][i+:8]);
+            end
+            $display(" |");
+        end
+
+        $display("\nB =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %d", u_matmul.matrix_B.ram[i][j+:8]);
+            end
+            $display(" |");
+        end
+    endtask
+
+    task automatic display_output_int8();
         $display("\nOutput =");
         for (int i = 0; i <= 24; i += 8) begin
             $write("|");
-	    for (int j = 0; j < 4; j++) begin
-                $write(" %d", u_matmul.matrix_C.ram[j][i+:8]);
-	    end
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %d", u_matmul.matrix_C.ram[j][i+:8]);
+            end
+            $display(" |");
+        end
+    endtask
+
+    task automatic display_inputs_fp8();
+        $display("\nA =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %f", fp8_to_real(u_matmul.matrix_A.ram[j][i+:8]));
+            end
+            $display(" |");
+        end
+
+        $display("\nB =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %f", fp8_to_real(u_matmul.matrix_B.ram[i][j+:8]));
+            end
+            $display(" |");
+        end
+    endtask
+
+    task automatic display_output_fp8();
+        $display("\nOutput =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                    $write(" %f", fp8_to_real(u_matmul.matrix_C.ram[j][i+:8]));
+            end
             $display(" |");
         end
     endtask
