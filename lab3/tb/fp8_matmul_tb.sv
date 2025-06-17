caa0cf73cb598a965b5a6148d848c85cb3465b83
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-17 02:47:55 -0700

    add fp8 matmul

---
+++ lab3/tb/fp8_matmul_tb.sv
@@ -0,0 +1,124 @@
+module matmul_tb;
+    // Set the timescale
+    timeunit 1ns;
+    timeprecision 100ps;
+
+    // DUT signals
+    logic clk;
+    logic resetn;
+    logic pe_resetn;
+    logic start;
+    wire done;
+
+    // DUT
+    matrix_multiplication u_matmul(
+        .clk(clk), 
+        .resetn(resetn),
+        .pe_resetn(pe_resetn), 
+        .address_mat_a(10'b0),
+        .address_mat_b(10'b0),
+        .address_mat_c(10'b0),
+        .address_stride_a(8'd1),
+        .address_stride_b(8'd1),
+        .address_stride_c(8'd1),
+        .start(start),
+	    .done(done)
+    );
+    
+    // Clock generation  
+    initial begin
+        clk = 1'b0;
+        forever #10 clk = ~clk;
+    end
+    
+    // Reset
+    initial begin
+        resetn = 1'b0;
+        pe_resetn = 1'b0;
+        #55;
+        resetn = 1'b1;
+        pe_resetn = 1'b1;
+    end
+    
+    // Perform test    
+    initial begin
+        `ifdef DUMP
+            $display("Dumping to FSDB");
+            $fsdbDumpvars();
+        `endif
+
+        set_matrices_fp8();
+        display_inputs_fp8();
+        start = 1'b0;
+        #115;
+        start = 1'b1;
+        @(posedge done);
+        start = 1'b0;
+        #100;         
+        display_output_fp8();
+
+        $display("\nAll done!");
+        $finish;
+    end
+    
+    //       A                               B                             Output
+    // +8.000 +4.000 +6.000 +8.000   +1.000 +1.000 +3.000 +0.000   +9.000 +9.000 +8.000 +3.000
+    // +3.000 +3.000 +3.000 +7.000   +0.000 +1.000 +4.000 +3.000   +7.000 +6.000 +5.000 +2.000
+    // +5.000 +2.000 +1.000 +6.000   +3.000 +5.000 +3.000 +1.000   +6.000 +4.000 +4.000 +1.000
+    // +9.000 +1.000 +0.000 +5.000   +9.000 +6.000 +3.000 +2.000   +5.000 +4.000 +4.000 +1.000
+    task automatic set_matrices_fp8();
+        //A is stored in ROW MAJOR format
+        //A[0][0] (8'h08) should be the least significant byte of ram[0]
+        //The first column of A should be read together. So, it needs to be 
+        //placed in the first matrix_A ram location.
+        //This is due to Verilog conventions declaring {MSB, ..., LSB}
+        u_matmul.matrix_A.ram[3]  = {8'h05, 8'h06, 8'h07, 8'h08}; 
+        u_matmul.matrix_A.ram[2]  = {8'h00, 8'h01, 8'h03, 8'h06};
+        u_matmul.matrix_A.ram[1]  = {8'h01, 8'h02, 8'h03, 8'h04};
+        u_matmul.matrix_A.ram[0]  = {8'h09, 8'h05, 8'h03, 8'h08};
+
+        //B is stored in COL MAJOR format
+        //B[0][0] (8'h01) should be the least significant of ram[0]
+        //The first row of B should be read together. So, it needs to be 
+        //placed in the first matrix_B ram location. 
+        u_matmul.matrix_B.ram[3]  = {8'h02, 8'h03, 8'h06, 8'h09};
+        u_matmul.matrix_B.ram[2]  = {8'h01, 8'h03, 8'h05, 8'h03};
+        u_matmul.matrix_B.ram[1]  = {8'h03, 8'h04, 8'h01, 8'h00};
+        u_matmul.matrix_B.ram[0]  = {8'h00, 8'h03, 8'h01, 8'h01};
+    endtask
+
+    task automatic display_inputs_fp8();
+        $display("\nA =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                $write(" ");
+                display_fp8(u_matmul.matrix_A.ram[j][i+:8]);
+            end
+            $display(" |");
+        end
+
+        $display("\nB =");
+        for (int i = 0; i < 4; i++) begin
+            $write("|");
+            for (int j = 0; j <= 24; j += 8) begin
+                $write(" ");
+                display_fp8(u_matmul.matrix_B.ram[i][j+:8]);
+            end
+            $display(" |");
+        end
+    endtask
+
+    task automatic display_output_fp8();
+        $display("\nOutput =");
+        for (int i = 0; i <= 24; i += 8) begin
+            $write("|");
+            for (int j = 0; j < 4; j++) begin
+                $write(" ");
+                display_fp8(u_matmul.matrix_C.ram[j][i+:8]);
+            end
+            $display(" |");
+        end
+    endtask
+
+endmodule
