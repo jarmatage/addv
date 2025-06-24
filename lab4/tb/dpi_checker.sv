63bb19f1c7bf39003ba7fa9713b6b4fb67a05eba
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-23 18:44:16 -0700

    update dpi_checker to hold sample code

--- lab4/tb/dpi_checker.sv
+++ lab4/tb/dpi_checker.sv
@@ -3,4 +3,14 @@ module dpi_checker (
     read_if  read       // Pop interface
 );
 
+    integer result;
+    always begin
+        @(posedge clk);
+        result = add(a, b);
+        $display("Result from C function: %0d", result);
+        @(negedge clk);
+        if (result != c) $error("Failed");
+        else $display("Pass");
+    end
+
 endmodule
