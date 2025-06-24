717f9655a86447cfc811a0d2a1b98399f0bb8e3b
Joaquin Armatage <j.armatage12@gmail.com>
2025-06-23 20:24:30 -0700

    update dpi.c

--- lab4/tb/dpi.c
+++ lab4/tb/dpi.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-int add(int a, int b) {
-    return a+b;
+#define DEPTH 16
+
+static uint8_t fifo[DEPTH];
+static int head = 0;  // write index
+static int tail = 0;  // read index
+static int count = 0;
+
+// Push value into FIFO
+void fifo_push(uint8_t data) {
+    if (count < DEPTH) {
+        fifo[head] = data;
+        head = (head + 1) % DEPTH;
+        count++;
+    } else {
+        printf("[C FIFO] push attempted when full!\n");
+    }
+}
+
+// Pop value from FIFO
+uint8_t fifo_pop() {
+    if (count > 0) {
+        uint8_t val = fifo[tail];
+        tail = (tail + 1) % DEPTH;
+        count--;
+        return val;
+    } else {
+        printf("[C FIFO] pop attempted when empty!\n");
+        return 0xFF;
+    }
+}
+
+// Query if FIFO is empty
+bool fifo_is_empty() {
+    return (count == 0);
+}
+
+// Query if FIFO is full
+bool fifo_is_full() {
+    return (count == DEPTH);
+}
+
+// Optional: Reset FIFO
+void fifo_reset() {
+    head = 0;
+    tail = 0;
+    count = 0;
 }
