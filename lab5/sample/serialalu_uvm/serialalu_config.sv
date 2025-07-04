106dee09a17a6c46b4e74f45bc31f0c965c892d6
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-04 07:57:18 -0700

    delete sample

--- lab5/sample/serialalu_uvm/serialalu_config.sv
+++
@@ -1,7 +0,0 @@
-class serialalu_configuration extends uvm_object;
-	`uvm_object_utils(serialalu_configuration)
-
-	function new(string name = "");
-		super.new(name);
-	endfunction: new
-endclass: serialalu_configuration
--- lab5/sample/serialalu_uvm/serialalu_if.sv
+++
@@ -1,9 +0,0 @@
-interface serialalu_if;
-	logic		sig_clock;
-	logic		sig_ina;
-	logic		sig_inb;
-	logic		sig_en_i;
-
-	logic		sig_out;
-	logic		sig_en_o;
-endinterface: serialalu_if
