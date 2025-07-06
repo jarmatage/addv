7f6d93dbbdc3780fb69f1860d743de93fcc8e5a9
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-06 06:05:56 -0700

    update params

--- lab6/src/common/apb_defines.svh
+++ lab6/src/common/apb_defines.svh
@@ -15,7 +15,7 @@
 `ifndef _APB_DEFINES_
 `define _APB_DEFINES_
 
-`define ADDR_WIDTH 32               // APB PADDR BUS width           
-`define DATA_WIDTH 32               // APB PWDATA and PRDATA Bus width
+`define ADDR_WIDTH 4                // APB PADDR BUS width           
+`define DATA_WIDTH 16               // APB PWDATA and PRDATA Bus width
 
 `endif
