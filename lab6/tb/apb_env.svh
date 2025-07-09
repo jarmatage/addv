e1c68dfa423ddae27f2f95030f8a0c79e3444e0d
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-09 04:01:43 -0700

    remove slave agent

--- lab6/tb/apb_env.svh
+++ lab6/tb/apb_env.svh
@@ -7,8 +7,7 @@ import apb_slave_pkg::*;
 class apb_env extends uvm_env;
 	`uvm_component_utils(apb_env)
 
-	apb_master_agent  master_agent;
-	apb_slave_agent  slave_agent;
+	apb_master_agent master_agent;
 
 	virtual apb_if vif;
 
@@ -23,7 +22,6 @@ class apb_env extends uvm_env;
 		super.build_phase(phase);
 		
 		master_agent = apb_master_agent::type_id::create("master_agent", this);
-		slave_agent  = apb_slave_agent::type_id::create("slave_agent", this);
 			
 		if (!uvm_config_db#(virtual apb_if)::get(null, "", "apb_vif", vif)) begin
 			`uvm_fatal(get_full_name(), "No virtual interface specified for env")
