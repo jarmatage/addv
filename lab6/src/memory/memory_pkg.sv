7db9789a9137a4a408e2e69b1a35c1148478e190
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-09 11:52:30 -0700

    rm seq

--- lab6/src/memory/memory_pkg.sv
+++ lab6/src/memory/memory_pkg.sv
@@ -4,7 +4,6 @@ package memory_pkg;
     `include "uvm_macros.svh"
     `include "memory_defines.svh"
     `include "memory_seq_item.svh"
-    `include "memory_seq.svh"
     `include "memory_sequencer.svh"
     `include "memory_driver.svh"
     `include "memory_slave_agent.svh"
