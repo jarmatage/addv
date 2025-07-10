ec2577043bc6cec1327d911a993f2f492f35fd50
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-09 18:10:44 -0700

    update apb env no ram

--- lab6/tb/apb_env_no_ram.svh
+++ lab6/tb/apb_env_no_ram.svh
@@ -4,8 +4,14 @@ class apb_env extends uvm_env;
 	`uvm_component_utils(apb_env)
 
 	apb_master_agent master_agent;
+	memory_slave_agent ram_a;
+	memory_slave_agent ram_b;
+	memory_slave_agent ram_c;
 
 	virtual apb_if vif;
+	virtual memory_if ram_a_vif;
+	virtual memory_if ram_b_vif;
+	virtual memory_if ram_c_vif;
 
 
 	function new (string name = "apb_env", uvm_component parent = null);
@@ -18,9 +24,21 @@ class apb_env extends uvm_env;
 		super.build_phase(phase);
 		
 		master_agent = apb_master_agent::type_id::create("master_agent", this);
+		ram_a = memory_slave_agent::type_id::create("ram_a", this);
+		ram_b = memory_slave_agent::type_id::create("ram_b", this);
+		ram_c = memory_slave_agent::type_id::create("ram_c", this);
 			
 		if (!uvm_config_db#(virtual apb_if)::get(null, "", "apb_vif", vif)) begin
 			`uvm_fatal(get_full_name(), "No virtual interface specified for env")
+		end
+		if (!uvm_config_db#(virtual memory_if)::get(null, "", "ram_a_vif", ram_a_vif)) begin
+			`uvm_fatal(get_full_name(), "No virtual interface specified for RAM A")
+		end
+		if (!uvm_config_db#(virtual memory_if)::get(null, "", "ram_b_vif", ram_b_vif)) begin
+			`uvm_fatal(get_full_name(), "No virtual interface specified for RAM B")
+		end
+		if (!uvm_config_db#(virtual memory_if)::get(null, "", "ram_c_vif", ram_c_vif)) begin
+			`uvm_fatal(get_full_name(), "No virtual interface specified for RAM C")
 		end
 	endfunction
 endclass
