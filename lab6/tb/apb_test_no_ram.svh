bc42857516ef61828b8c06a8658f7041a4fc7842
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-10 21:39:13 -0700

    f

--- lab6/tb/apb_test_no_ram.svh
+++ lab6/tb/apb_test_no_ram.svh
@@ -305,9 +305,9 @@ class apb_test extends uvm_test;
 					diff = actual_fp8 - expected_fp8;
 
 				if (diff > 1)
-					`uvm_error("CHECK_RESULTS", $sformatf("Mismatch at C[%0d][%0d]: expected %b, got %b", i, j, expected_fp8, actual_fp8))
+					`uvm_error("CHECK_RESULTS", $sformatf("Mismatch at C[%0d][%0d]: expected %b, got %b, diff = %0d", i, j, expected_fp8, actual_fp8, diff))
 				else
-					`uvm_info("CHECK_RESULTS", $sformatf("Match at C[%0d][%0d]: expected %b, got %b, diff = %d", i, j, expected_fp8, actual_fp8, diff), UVM_LOW)
+					`uvm_info("CHECK_RESULTS", $sformatf("Match at C[%0d][%0d]: expected %b, got %b", i, j, expected_fp8, actual_fp8), UVM_LOW)
 			end
 		end
 	endtask
