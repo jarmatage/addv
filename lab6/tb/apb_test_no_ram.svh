ac8721ae8e2f477ee87dc6260a0dea5d5984b530
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-10 21:30:25 -0700

    f

--- lab6/tb/apb_test_no_ram.svh
+++ lab6/tb/apb_test_no_ram.svh
@@ -304,7 +304,7 @@ class apb_test extends uvm_test;
 				else
 					diff = actual_fp8 - expected_fp8;
 
-				if (diff > 2)
+				if (diff > 1)
 					`uvm_error("CHECK_RESULTS", $sformatf("Mismatch at C[%0d][%0d]: expected %b, got %b", i, j, expected_fp8, actual_fp8))
 				else
 					`uvm_info("CHECK_RESULTS", $sformatf("Match at C[%0d][%0d]: expected %b, got %b", i, j, expected_fp8, actual_fp8), UVM_LOW)
