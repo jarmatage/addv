fa32a73925e88d04362d04a4546cc53527be2058
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-12 23:04:17 -0700

    move write driver to own file

--- final/tb/write_agent.svh
+++ final/tb/write_agent.svh
@@ -1,29 +1,3 @@
-class write_driver extends uvm_driver #(fifo_seq_item);
-  virtual write_if #(8,4) vif;
-  `uvm_component_utils(write_driver)
-
-  function new(string n, uvm_component p); super.new(n,p); endfunction
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    if(!uvm_config_db#(virtual write_if#(8,4))::get(this,"","vif",vif))
-      `uvm_fatal("NOVIF","write_if not set")
-  endfunction
-
-  task run_phase(uvm_phase phase);
-    fifo_seq_item item;
-    forever begin
-      seq_item_port.get_next_item(item);
-      @(negedge vif.clk);
-      vif.en   <= 1;
-      vif.data <= item.data;
-      @(posedge vif.clk);
-      vif.en   <= 0;
-      seq_item_port.item_done();
-    end
-  endtask
-endclass
-
 class write_agent extends uvm_agent;
   `uvm_component_utils(write_agent)
 
