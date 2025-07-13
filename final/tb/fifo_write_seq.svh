657181417b4ebdc632262b85cd70b85fe0a9262b
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-13 06:57:07 -0700

    d

--- final/tb/fifo_write_seq.svh
+++ final/tb/fifo_write_seq.svh
@@ -7,7 +7,7 @@ class fifo_write_seq extends uvm_sequence #(fifo_seq_item);
   task body();
     fifo_seq_item item;
     repeat (burst_len) begin
-      $display("are you there???????")
+      $display("are you there???????");
       `uvm_create(item)
       item.is_write = 1;
       assert(item.randomize() with { data dist {0  :=1, 255 :=1, [1:254]:=98}; });
