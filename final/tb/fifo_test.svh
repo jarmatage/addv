431e368ba4158e7975fd81ad4b409ef4d9c3059b
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-13 07:46:29 -0700

    add fill for loop

--- final/tb/fifo_test.svh
+++ final/tb/fifo_test.svh
@@ -1,6 +1,8 @@
 class fifo_test extends uvm_test;
   `uvm_component_utils(fifo_test)
 
+  const static int DEPTH = (1 << `AWIDTH);
+
   fifo_env m_env;
   fifo_write_seq wr_seq;
   fifo_read_seq rd_seq;
@@ -15,14 +17,23 @@ class fifo_test extends uvm_test;
   endfunction
 
   task run_phase(uvm_phase phase);
-    wr_seq = fifo_write_seq::type_id::create("wr_seq");
-    rd_seq = fifo_read_seq::type_id::create("rd_seq");
     phase.raise_objection(this);
     fill_for_loop();
-    alternating_read_write();
-    random_sequential_burts();
-    random_simultaneous_burts();
-    wr_seq.start(m_env.w_ag.m_seqr); // start random writes
+    //alternating_read_write();
+    //random_sequential_burts();
+    //random_simultaneous_burts();
     phase.drop_objection(this);
   endtask
+
+  task fill_for_loop();
+    for (int i = 0; i < DEPTH; i++) begin
+      wr_seq = fifo_write_seq::type_id::create("wr_seq");
+      rd_seq = fifo_read_seq::type_id::create("rd_seq");
+      wr_seq.burst_len = i;
+      rd_seq.burst_len = i;
+      wr_seq.start(m_env.w_ag.m_seqr);
+      rd_seq.start(m_env.r_ag.m_seqr);
+    end
+  endtask
+
 endclass
