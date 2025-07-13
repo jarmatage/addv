a00f9df5a1864ddb833f3c9adfef726a5d5e1e21
Joaquin Armatage <j.armatage12@gmail.com>
2025-07-13 04:21:04 -0700

    only compile_ultra

--- final/synth/synth.tcl
+++ final/synth/synth.tcl
@@ -1,7 +1,24 @@
 # Async Fifo Design Compiler Run Script
-lappend search_path [file join $::env(PDK_DIR) osu_soc lib files]
+proc setup_clocks {{wperiod 1.0} {rperiod 1.0}} {
+    # Remove existing clocks
+    remove_clock -all
+
+    # Create new clocks with specified periods
+    create_clock -period $wperiod write.clk
+    create_clock -period $rperiod read.clk
+    set clock_uncertainty 0.1 [get_clocks]
+
+    # Set delay on the ports
+    set inputs [all_inputs -exclude_clock_ports]
+    set_input_delay 0.1 -clock write.clk [filter_collection $inputs {name=~write.*}]
+    set_input_delay 0.1 -clock read.clk  [filter_collection $inputs {name=~read.*}]
+    set outputs [all_outputs]
+    set_output_delay 0.1 -clock write.clk [filter_collection $outputs {name=~write.*}]
+    set_output_delay 0.1 -clock read.clk  [filter_collection $outputs {name=~read.*}]
+}
 
 # Set libraries
+lappend search_path [file join $::env(PDK_DIR) osu_soc lib files]
 set alib_library_analysis_path "."
 set link_library [list gscl45nm.db dw_foundation.sldb]
 set target_library [list gscl45nm.db]
@@ -24,21 +41,10 @@ set_optimize_registers true
 # Set the driving cell for all input ports
 set_driving_cell -lib_cell INVX1 [all_inputs]
 
-# Create read and write clocks
-create_clock -period 1.0 write.clk;
-create_clock -period 1.0 read.clk;
-
-# Set delay on the ports
-set inputs [all_inputs -exclude_clock_ports]
-set_input_delay 0.1 -clock write.clk [filter_collection $inputs {name=~write.*}]
-set_input_delay 0.1 -clock read.clk  [filter_collection $inputs {name=~read.*}]
-set outputs [all_outputs]
-set_output_delay 0.1 -clock write.clk [filter_collection $outputs {name=~write.*}]
-set_output_delay 0.1 -clock read.clk  [filter_collection $outputs {name=~read.*}]
+setup_clocks 0.8 0.8
 
 # Compile
-compile -ungroup_all -map_effort high
-compile_ultra -incremental -timing_high_effort_script
+compile_ultra -timing_high_effort_script
 
 # Validate
 check_design
